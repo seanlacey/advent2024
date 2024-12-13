e297cdd54f9fb145682ef2b8bd537b7a8d2b07f8
Sean Lacey <48262081+seanlacey@users.noreply.github.com>
2024-12-13 12:09:14 -0500

    Create day1.R
    I'm going through and adding my current solutions (just the code) so I can have it for posterity

---
+++ day1.R
@@ -0,0 +1,31 @@
+library(magrittr)
+library(dplyr)
+library(purrr)
+
+### Total Distnace
+in_data <- read.table("day1/data/input.txt")
+
+list1 <- in_data[, 1] %>%
+  sort
+
+list2 <- in_data[, 2] %>%
+  sort
+
+total_distance <- as.numeric(sum(abs(list1-list2)))
+
+
+# Similarity Score --------------------------------------------------------
+### Create Table of Frequencies of list2
+freq_list2 <- table(list2)
+
+###Create Function for Apply
+f_score <- function(x, flist = freq_list2) {
+
+  score <- x * flist[names(flist) == x]
+  
+  return(score)
+}
+
+score_list <- as.numeric(sapply(list1, f_score))
+
+total_score <- sum(score_list, na.rm = TRUE)
